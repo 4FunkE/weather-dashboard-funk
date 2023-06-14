a91d1d00fefc87ee29ea7ec6a852e59887c5d661
4FunkE <4FunkEcodes@gmail.com>
2023-06-14 18:01:24 -0400

    scaffolding psuedo code

---
+++ assets/script.js
@@ -0,0 +1,18 @@
+//Your API key is 8eea77c5d98f1a676f012632c1fa37ad
+
+// Endpoint:
+// - Please, use the endpoint api.openweathermap.org for your API calls
+// - Example of API call:
+// api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=8eea77c5d98f1a676f012632c1fa37ad
+// Useful links:
+// - API documentation https://openweathermap.org/api
+
+// GIVEN a weather dashboard with form inputs
+// WHEN I search for a city
+// THEN I am presented with current and future conditions for that city and that city is added to the search history
+// WHEN I view current weather conditions for that city
+// THEN I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, and the the wind speed
+// WHEN I view future weather conditions for that city
+// THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, the wind speed, and the humidity
+// WHEN I click on a city in the search history
+// THEN I am again presented with current and future conditions for that city
