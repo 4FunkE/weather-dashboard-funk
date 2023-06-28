81444522eb19c13b08ff9cafbd01990750a8cf2a
4FunkE <4FunkEcodes@gmail.com>
2023-06-28 13:25:41 -0400

    created a fetch call, debugging missing code

--- assets/script.js
+++ assets/script.js
@@ -12,7 +12,7 @@ const forecast = document.getElementById("forecast");
 // Useful links:
 // - API documentation https://openweathermap.org/api
 
-// WHEN I search for a city
+// event listener for search button
 searchForm.addEventListener("submit", function (event) {
     event.preventDefault();
     const city = cityInput.value.trim();
@@ -20,12 +20,36 @@ searchForm.addEventListener("submit", function (event) {
         getWeather(city);
         cityInput.value = "";
     }
-    console.log("clicked")
+    console.log("clicked search for city!")
 });
-// THEN I am presented with current and future conditions for that city and that city is added to the search history
+//fetch to get weather from API
+function getWeather(city) {
+    const weatherURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=${API_KEY}&units=metric`;
+    fetch(weatherURL)
+        .then(response => response.json())
+        .then(data => {
+            displayCurrentWeather(data);//need what the weather will display
+            addSearchHistory(city);
+        })
+        .catch(error => {
+            console.log("Error:", error);
+        });
+
+    const forecastURL = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&APPID=${API_KEY}&units=metric`;
+    fetch(forecastURL)
+        .then(response => response.json())
+        .then(data => {
+            displayForecast(data);//need what the weather will display
+        })
+        .catch(error => {
+            console.log("Error:", error);
+        });
+}
 // WHEN I view current weather conditions for that city
 // THEN I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, and the the wind speed
+
 // WHEN I view future weather conditions for that city
 // THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, the wind speed, and the humidity
+
 // WHEN I click on a city in the search history
 // THEN I am again presented with current and future conditions for that city
