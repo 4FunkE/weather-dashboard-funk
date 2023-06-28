07b37465e9cab97f7eb6ae4ac11e575662dc7935
4FunkE <4FunkEcodes@gmail.com>
2023-06-28 13:14:43 -0400

    style & script, not linked correctly/debug search

--- assets/script.js
+++ assets/script.js
@@ -12,8 +12,16 @@ const forecast = document.getElementById("forecast");
 // Useful links:
 // - API documentation https://openweathermap.org/api
 
-// GIVEN a weather dashboard with form inputs
 // WHEN I search for a city
+searchForm.addEventListener("submit", function (event) {
+    event.preventDefault();
+    const city = cityInput.value.trim();
+    if (city) {
+        getWeather(city);
+        cityInput.value = "";
+    }
+    console.log("clicked")
+});
 // THEN I am presented with current and future conditions for that city and that city is added to the search history
 // WHEN I view current weather conditions for that city
 // THEN I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, and the the wind speed
